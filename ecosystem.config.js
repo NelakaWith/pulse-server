feaece14a3acc0d1c47db372d64f803998faa2fc
NelakaWith <nelaka.with@gmail.com>
2025-11-18 08:21:38 +0530

    fix(config): update PORT to 3002 for development and production environments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
       // Environment variables
       env: {
         NODE_ENV: "development",
-        PORT: 3000,
+        PORT: 3002,
       },
       env_production: {
         NODE_ENV: "production",
-        PORT: 3000,
+        PORT: 3002,
       },
 
       // Logging
